f1d5e2ef47b4d18297b00c1656d4803377256c36
yugal oli <yugaloli123@gmail.com>
2018-03-07 16:58:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 //counter
 var counter=0;
-button = document.getElementById("counter");
+button = document.getElementById('counter');
 button.onClick= function (){
     counter=counter+1;
-    span=document.getElementById("count");
+    span=document.getElementById('count');
     span.innerHTML=counter.toString();
-}
+};

7492bcb6f5975a07a588c0d6fdd85acf7d8f4e34
yugal oli <yugaloli123@gmail.com>
2018-03-07 17:25:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,6 +14,7 @@ button.onclick= function (){
             
         }
     };
-    request.open('GET','http://yugaloli123.imad.hasura-app.io/counter')
+    request.open('GET','http://yugaloli123.imad.hasura-app.io/counter');
+    request.send(null);
    
 };

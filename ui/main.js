154b6fed413abaab319adfd717f6cd65607e3140
yugal oli <yugaloli123@gmail.com>
2018-03-07 16:56:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+//counter
+var counter=0;
+button = document.getElementById("counter");
+button.onClick= function (){
+    counter=counter+1;
+    span=document.getElementById("span");
+    span.innerHTML=counter.toString();
+}
